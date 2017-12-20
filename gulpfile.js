b6780251e585518335831876547e1880b3f6d0bb
Mikolaj Stankowiak <mikolaj.stankowiak@gmail.com>
2017-12-20 21:34:12 +0100

    changes

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,7 @@
 const gulp = require('gulp');
 const $ = require('gulp-load-plugins')({lazy: true});
-const rollupBabel = require('rollup-plugin-babel');
 
-const babelOptions = {
-    presets: [['env', {modules: false}]],
-    plugins: ['external-helpers']
-};
-const rollupOptions = {
-    plugins: [rollupBabel(babelOptions)]
-};
+const rollupOptions = {};
 const generateOptions = {format: 'iife'};
 
 gulp.task('default', () => {
